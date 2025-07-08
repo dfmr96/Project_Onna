602824f4d3b52bf9a4684f3466c97ecd8a5dd5d3
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-07-08 19:33:35 -0300

    Update Pillar.cs

--- Assets/Scripts/Enemy/Boss/Pillar.cs
+++ Assets/Scripts/Enemy/Boss/Pillar.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using static UnityEditor.PlayerSettings;
 
 public class Pillar : MonoBehaviour, IDamageable
 {
