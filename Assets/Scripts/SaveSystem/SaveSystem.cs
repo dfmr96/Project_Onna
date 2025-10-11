fceea228e269804851392bcfde1939d946ec3fe5
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-10-11 14:59:09 -0300

    Save system now encrypted

--- Assets/Scripts/SaveSystem/SaveSystem.cs
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -1,23 +1,38 @@
+using System;
 using System.IO;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public static class SaveSystem
 {
-    private static string Path => Application.persistentDataPath + "/saveData.json";
+    private static readonly string SavePath = Application.persistentDataPath + "/saveData.save";
+
+    private static readonly string EncryptionKeyBase64 = "Ezy5BxVtShP5Q0iU+YGlBg==";
+    private static readonly string EncryptionIVBase64 = "Wb6YkR2N8mLq9FfG4tK1TQ==";
 
     public static SaveData Load()
     {
-        if (!File.Exists(Path))
+        if (!File.Exists(SavePath))
             return new SaveData();
 
-        string json = File.ReadAllText(Path);
-        return JsonUtility.FromJson<SaveData>(json);
+        try
+        {
+            byte[] encryptedBytes = File.ReadAllBytes(SavePath);
+            string decryptedJson = Decrypt(encryptedBytes);
+            return JsonUtility.FromJson<SaveData>(decryptedJson);
+        }
+        catch (Exception) { return new SaveData(); }
     }
 
     public static void Save(SaveData data)
     {
-        string json = JsonUtility.ToJson(data, true);
-        File.WriteAllText(Path, json);
+        try
+        {
+            string json = JsonUtility.ToJson(data, true);
+            byte[] encryptedBytes = Encrypt(json);
+            File.WriteAllBytes(SavePath, encryptedBytes);
+        }
+        catch (Exception) { }
     }
 
     public static void SaveInventory(PlayerInventory newInventory)
@@ -39,5 +54,35 @@ public static class SaveSystem
         var data = Load();
         data.progress.hasSeenIntro = true;
         Save(data);
+    }
+
+    private static byte[] Encrypt(string plainText)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(EncryptionKeyBase64);
+            aes.IV = Convert.FromBase64String(EncryptionIVBase64);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                using (StreamWriter sw = new StreamWriter(cs)) { sw.Write(plainText); }
+
+                return ms.ToArray();
+            }
+        }
+    }
+
+    private static string Decrypt(byte[] cipherBytes)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(EncryptionKeyBase64);
+            aes.IV = Convert.FromBase64String(EncryptionIVBase64);
+
+            using (MemoryStream ms = new MemoryStream(cipherBytes))
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+            using (StreamReader sr = new StreamReader(cs)) { return sr.ReadToEnd(); }
+        }
     }
 }
