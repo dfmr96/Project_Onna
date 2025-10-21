6b4b5efe5335d304d7f5cea40a4c387a305e232e
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-10-21 15:30:43 -0300

    Update LevelEndTrigger.cs

--- Assets/Scripts/LevelEndTrigger.cs
+++ Assets/Scripts/LevelEndTrigger.cs
@@ -16,7 +16,8 @@ public class LevelEndTrigger : LevelTrigger
     private void Start()
     {
         //Evento para activar portal tras seleccion de mutacion
-        GameManager.OnMutationUIClosed += ActivatePortal;
+        if (GameManager.Instance != null)
+            GameManager.OnMutationUIClosed += ActivatePortal;
 
     }
 
@@ -40,7 +41,8 @@ public class LevelEndTrigger : LevelTrigger
 
         Instantiate(portalPrefab, position, rotation);
 
-        GameManager.OnMutationUIClosed -= ActivatePortal;
+        if (GameManager.Instance != null)
+            GameManager.OnMutationUIClosed -= ActivatePortal;
 
     }
 }
